f1c69434adceb97fb1f8b9bee474c869a6713b9a
SS56Bo <sohamsarkar560@gmail.com>
2024-05-25 10:25:09 +0530

    Added Prettier config

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const canvas = document.querySelector("element");
-const c = canvas.getContext("2d");
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
-canvas.width = 1024;
-canvas.height = 576;
+canvas.width = 1280;
+canvas.height = 650;
+
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+  constructor(position) {
+    this.position = position;
+  }
+}
+
+const player = new Sprite({
+  x: 0,
+  y: 0,
+});
+
+console.log(player);
