977b21cd656d6995229fbeba45339a9044f86b6c
SS56Bo <sohamsarkar560@gmail.com>
2024-05-25 12:14:59 +0530

    Added New Features and Updated Roadmap

--- index.js
+++ index.js
@@ -1,32 +1,72 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
+let gravity = 0.2;
 canvas.width = 1280;
-canvas.height = 650;
+canvas.height = 580;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 class Sprite {
-  constructor(position) {
+  constructor({ position, velocity }) {
+    // Constrcutor for player position
     this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
   }
 
   draw(fillStyle) {
-    c.fillStyle = fillStyle;
+    c.fillStyle = fillStyle; //Constructor for player color and drawing [JUST A FORMALITY]
     c.fillRect(this.position.x, this.position.y, 50, 150);
+  }
+
+  update(fillStyle) {
+    this.draw(fillStyle);
+
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
+    }
   }
 }
 
 const player = new Sprite({
-  x: 0,
-  y: 0,
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 0.5,
+  },
 });
 
 player.draw('red');
 
 const enemy = new Sprite({
-  x: 400,
-  y: 100,
+  position: {
+    x: 100,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
 });
 
 enemy.draw('blue');
+
+animate = () => {
+  //arrow function for creating animate function
+  window.requestAnimationFrame(animate);
+  c.fillStyle = 'black';
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update('red');
+  enemy.update('green');
+  console.log('LFG');
+};
+
+animate(); //creates a constant recursive infinite loop
